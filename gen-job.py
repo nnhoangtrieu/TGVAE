6ce0f20cb056e94854e598a2220d65ac00443cef
Trieu.Nguyen <80027464@hpctpa3pl0001.cm.cluster>
2024-02-17 11:02:07 -0500

    add genjob

--- gen-job.py
+++ gen-job.py
@@ -1,37 +1,62 @@
-scripts = ['python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.001 --kl_cycle 2 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 2 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.001 --kl_cycle 4 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 6 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 4 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_w_start 0.0001 --kl_w_end 0.01 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.001 --kl_cycle 2 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 2 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.001 --kl_cycle 4 --kl_ratio 1.0 --save_name "43up"',
-'python train.py --max_len 57 --batch 128 --n_layers 8 --n_epochs 100 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 4 --kl_ratio 1.0 --save_name "43up"'
+import re 
+
+
+
+
+scripts = [
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 4 --kl_ratio 1',
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 8 --kl_ratio 1',
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.01 --kl_cycle 10 --kl_ratio 1',
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.005 --kl_cycle 4 --kl_ratio 1',
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.005 --kl_cycle 8 --kl_ratio 1',
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "cyclic" --kl_w_start 0.0001 --kl_w_end 0.005 --kl_cycle 10 --kl_ratio 1',
+
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "monotonic" --kl_w_start 0.0001 --kl_w_end 0.01'
+'train-mulnode.py --max_len 100 --batch 1024 --n_layers 8 --lr 0.00025 --kl_type "monotonic" --kl_w_start 0.0001 --kl_w_end 0.05'
 ]
 
 
-for i in range(len(scripts)) : 
+check = []
+
+for i, script in enumerate(scripts) :
+    split = script.split('--')[1:]
+    batch = int(re.findall(f'--batch (\d+)',script)[0])
+    print("Batch not divisible by 128") if batch % 128 != 0 else None
+    split = [s.replace(' ','').replace('"','') for s in split]
+    save_name = ' '.join(split)
+    if save_name in check : 
+        print('Name existed')
+        exit()
+    else :
+        check.append(save_name)
+
     with open(f'job{i}.sub', 'w') as f : 
         f.write(f'''#!/bin/bash
-#SBATCH --ntasks 1
+#SBATCH --job-name=trieu-nguyen
 #SBATCH --time 3-0
-#SBATCH -c 4
-#SBATCH --mem 40g
-#SBATCH --output 43up/job-out/%j.out
+#SBATCH --nodes={batch // 128}
+#SBATCH --ntasks={batch // 128}
 #SBATCH --gres=gpu:a30:1
-                
+#SBATCH --cpus-per-task=2
+#SBATCH --output=job-out/{'|'.join(split)}.out
+
+export MASTER_ADDR=$(scontrol show hostname ${{SLURM_NODELIST}} | head -n 1)
+
 ml Python 
-{scripts[i]}
+srun python -m torch.distributed.launch --use_env \
+--nnodes {batch // 128} \
+--nproc_per_node 1 \
+--rdzv_id $RANDOM \ 
+--rdzv_backend c10d \ 
+--rdzv_endpoint $MASTER_ADDR:29500 \ 
+{script} --save-name "{save_name}"
  ''')
         
 
-# for file in /home/80027464/graphvae/job*; do [ -f "$file" ] && sbatch "$file"; done
 
+
+
+
+
+# for file in /home/80027464/graphvae/job*; do [ -f "$file" ] && sbatch "$file"; done
 
