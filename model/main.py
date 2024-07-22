14bda4c32b766f0458688b7cb75d393771a29655
nnhoangtrieu <107376082+nnhoangtrieu@users.noreply.github.com>
2024-07-22 09:19:09 -0400

    Add files via upload

--- model/main.py
+++ model/main.py
@@ -1,1 +1,186 @@
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+import torch_geometric.nn as gnn
+import sys 
+sys.path.append('/home/80027464/TGVAE/model/')
+from sublayer import * 
 
+
+
+class EncoderLayer(nn.Module) : 
+    def __init__(self, d_model, d_ff, edge_dim, e_head, dropout, gnn_mode='res') : 
+        super(EncoderLayer, self).__init__()
+        self.gnn_mode = gnn_mode
+        self.norm1 = LayerNorm(d_model)
+        self.attn1 =  gnn.GATConv(d_model, d_model//e_head, heads=e_head, dropout=dropout, edge_dim=edge_dim)
+        self.drop1 = nn.Dropout(dropout)
+
+        self.norm2 = LayerNorm(d_model)
+        self.attn2 = gnn.GATConv(d_model, d_model//e_head, heads=e_head, dropout=dropout, edge_dim=edge_dim)
+        self.drop2 = nn.Dropout(dropout)
+
+        self.norm3 = LayerNorm(d_model)
+        self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout = dropout)
+        self.drop3 = nn.Dropout(dropout)
+
+    def forward(self, nf, ei, ew) : 
+        if self.gnn_mode == 'res' : 
+            nf = nf + F.leaky_relu(self.norm1(self.attn1(self.drop1(nf), ei, ew)))
+            nf = nf + F.leaky_relu(self.norm2(self.attn2(self.drop2(nf), ei, ew)))
+            nf = nf + F.leaky_relu(self.norm3(self.feed_forward(self.drop3(nf))))
+
+        elif self.gnn_mode == 'res+' : 
+            nf = nf + self.attn1(self.drop1(F.leaky_relu(self.norm1(nf))), ei, ew)
+            nf = nf + self.attn2(self.drop2(F.leaky_relu(self.norm2(nf))), ei, ew)
+            nf = nf + self.feed_forward(self.drop3(F.leaky_relu(self.norm3(nf))))
+
+        return nf
+    
+
+
+class Encoder(nn.Module) :
+    def __init__(self, d_model, d_ff, edge_dim, e_head, dropout, n_layer, encoder_mode='none', pool_mode='add', gnn_mode='res6_relu') : 
+        super(Encoder, self).__init__()
+        self.encoder_mode = encoder_mode
+        self.pool_mode = pool_mode
+        self.layers = clones(EncoderLayer(d_model, d_ff, edge_dim, e_head, dropout, gnn_mode), n_layer)
+        self.norm = LayerNorm(d_model)
+
+    def forward(self, nf, ei, ew, batch) : 
+        if self.encoder_mode == 'dense' : 
+            dense = nf 
+            for layer in self.layers : 
+                nf = layer(nf, ei, ew) + dense 
+                dense = nf + dense
+        elif self.encoder_mode == 'res' : 
+            res = nf 
+            for layer in self.layers : 
+                nf = layer(nf, ei, ew) + res 
+                res = nf 
+        elif self.encoder_mode == 'none' : 
+            for layer in self.layers : 
+                nf = layer(nf, ei, ew)
+
+        nf = self.norm(nf)
+
+        if self.pool_mode == 'add' : 
+            pool = gnn.global_add_pool(nf, batch)
+        elif self.pool_mode == 'mean' : 
+            pool = gnn.global_mean_pool(nf, batch)
+        elif self.pool_mode == 'max' :
+            pool = gnn.global_max_pool(nf, batch)            
+
+        return pool
+    
+
+
+
+class LatentModel(nn.Module) : 
+    def __init__(self, d_model) : 
+        super(LatentModel, self).__init__()
+        self.mu = nn.Linear(d_model, d_model)
+        self.sigma = nn.Linear(d_model, d_model)
+
+    def forward(self, x) : 
+        mu, sigma = self.mu(x), self.sigma(x)
+        eps = torch.rand_like(sigma).to(device)
+        z = mu + torch.exp(0.5 * sigma) * eps
+        return z, mu, sigma
+
+
+
+
+class DecoderLayer(nn.Module) : 
+    def __init__(self, d_model, d_ff, d_head, dropout) : 
+        super(DecoderLayer, self).__init__()
+        self.self_attn = MultiHeadedAttention(d_head, d_model, dropout) 
+        self.src_attn = MultiHeadedAttention(d_head, d_model, dropout)
+        self.feed_forward = PositionwiseFeedForward(d_model, d_ff, dropout)
+        self.sublayer = clones(SublayerConnection(d_model, dropout), 3)
+    
+    def forward(self, x, memory, smi_mask) : 
+        x = self.sublayer[0](x, lambda x : self.self_attn(x, x, x, smi_mask))
+        x = self.sublayer[1](x, lambda x : self.src_attn(x, memory, memory, None))
+        return self.sublayer[2](x, self.feed_forward)
+
+
+
+
+
+class Decoder(nn.Module) :
+    def __init__(self, d_model, d_ff, d_head, dropout, n_layer) : 
+        super(Decoder, self).__init__() 
+        self.layers = clones(DecoderLayer(d_model, d_ff, d_head, dropout), n_layer)
+        self.norm = LayerNorm(d_model)
+    
+    def forward(self, x, memory, smi_mask) : 
+        for layer in self.layers : 
+            x = layer(x, memory, smi_mask)
+        return self.norm(x)
+    
+
+
+
+class TGVAE(nn.Module) : 
+    def __init__(self,
+                 d_model=[512, 512],
+                 d_ff=1024, 
+                 edge_dim=None,
+                 n_head=[1, 16],
+                 dropout=0.5,
+                 n_layer=[8, 8],
+                 node_vocab_size=0,
+                 smi_vocab_size=0,
+                 edge_vocab_size=4,
+                 encoder_mode='dense',
+                 pool_mode='add',
+                 gnn_mode='res6_relu') : 
+        
+        super(TGVAE, self).__init__()
+        self.d_model = d_model
+
+        self.node_vocab_size = node_vocab_size
+        self.edge_vocab_size = edge_vocab_size
+
+        self.node_embedding = gnn.GATConv(node_vocab_size, d_model[0]//n_head[0], n_head[0], dropout = dropout)
+        self.norm = LayerNorm(d_model[0])
+        self.smi_embedding = nn.Sequential(Embeddings(d_model[1], smi_vocab_size), PositionalEncoding(d_model[1], dropout))
+
+
+        self.encoder = Encoder(d_model[0], d_ff, edge_dim, n_head[0], dropout, n_layer[0], encoder_mode, pool_mode, gnn_mode)
+        self.latent_model = LatentModel(d_model[0]) 
+        self.decoder = Decoder(d_model[1], d_ff, n_head[1], dropout, n_layer[1])
+
+
+        self.generator = nn.Linear(d_model[1], smi_vocab_size)
+
+
+
+    def inference(self, z, smi, smi_mask) : 
+        smi = self.smi_embedding(smi)
+        out = self.decoder(smi, z, smi_mask)
+        out = F.log_softmax(self.generator(out), dim = -1)
+        return out
+    
+    
+    def forward(self, graph, smi, smi_mask) : 
+        node_feature, edge_index, edge_weight, batch = graph.x, graph.edge_index, graph.edge_attr, graph.batch
+
+        node_feature = (F.one_hot(node_feature, num_classes=self.node_vocab_size)).float()
+        edge_weight = (F.one_hot(edge_weight, num_classes=self.edge_vocab_size)).float()
+
+        node_feature = F.leaky_relu(self.node_embedding(node_feature, edge_index, edge_weight))
+
+        smi = self.smi_embedding(smi)
+
+        pool = self.encoder(node_feature, edge_index, edge_weight, batch)
+
+        z, mu, sigma = self.latent_model(pool)
+        
+        out = self.decoder(smi, z, smi_mask)
+
+        out = F.log_softmax(self.generator(out), dim = -1)
+
+        return out, mu, sigma
+    
---
+++ model/sublayer.py
@@ -0,0 +1,115 @@
+import math 
+import copy 
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+def attention(query, key, value, mask=None, dropout=None):
+    "Compute 'Scaled Dot Product Attention'"
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = scores.softmax(dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+class SublayerConnection(nn.Module):
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class PositionwiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.leaky_relu(self.w_1(x))))
+    
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        nbatches = query.size(0)
+
+        query, key, value = [
+            lin(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+            for lin, x in zip(self.linears, (query, key, value))
+        ]
+
+        x, self.attn = attention(
+            query, key, value, mask=mask, dropout=self.dropout
+        )
+
+        x = (
+            x.transpose(1, 2)
+            .contiguous()
+            .view(nbatches, -1, self.h * self.d_k)
+        )
+        del query
+        del key
+        del value
+        return self.linears[-1](x)
+    
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
+        return self.dropout(x)
+    
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
